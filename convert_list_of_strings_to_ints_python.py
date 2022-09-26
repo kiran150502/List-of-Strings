a0e44e45ee78b8e029972726600868b05a2ee863
K Kiran Kumar <113544336+kiran150502@users.noreply.github.com>
2022-09-26 23:13:11 +0530

    Add files via upload

---
+++ convert_list_of_strings_to_ints_python.py
@@ -0,0 +1,33 @@
+# a=['1', '4', '3', '6', '7']
+# b=[int(v) for v in a]
+# print(b)
+
+# w=['10','20','30']
+# print(list(map(int, w)))
+
+# a = ["1", "2", "3"]
+# ints = []
+# for i in a:
+#     ints.append(int(i))
+#     print(i)
+
+# a = ["1", "2", "-3"]
+# ints = [eval(x) for x in a]
+# print(ints)
+
+# a = ["1", "2.0", "-3.4", "3.6"]
+# ints = [round(float(s)) for s in a]
+# print(ints)
+
+
+
+
+
+
+
+
+
+
+
+# b= [int(i) for i in a]
+# print(b)
